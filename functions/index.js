a7a5e222709316b67e5fbc7b6e040cfbb58e5d80
Marein <mail@marein.net>
2021-02-12 14:12:02 +0100

    seedrandom fix

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+require('seedrandom');
 admin.initializeApp(functions.config().firebase);
 
 /////////////////
